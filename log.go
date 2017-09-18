b1dd9619997658e9581dceb311c5dd18bfc0bc2a
Albert Jin <albert.jin@gmail.com>
2017-09-18 11:24:45 +0800

    use httputil2_log environment variable to control log output

--- log.go
+++ log.go
@@ -1,13 +1,15 @@
 package httputil2
 
 import (
+    "os"
+
     "github.com/albertjin/log2"
 )
 
 var critical = log2.Critical
 
 var Logger = log2.NewStdLogger(nil)
-var LogDebug = false
+var LogDebug = (os.Getenv("httputil2_log") == "1")
 
 func log(a... interface{}) {
     Logger.Output(LogDebug, 1, a)
